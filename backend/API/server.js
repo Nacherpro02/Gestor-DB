8cdc93f6d143281cec51c8e954f288f4e9de12bb
Mohammad Nacher Rahman Akter <154415872+Nacherpro02@users.noreply.github.com>
2025-04-09 21:06:02 +0200

    update log in out

--- backend/API/server.js
+++ backend/API/server.js
@@ -4,20 +4,18 @@ const app = express();
 require('dotenv').config();
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500',
-  credentials: true
+  origin: 'http://localhost:5500',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'x-auth-token'],
+  credentials: true,
 }));
-
-
-
 
 app.use(express.json());
 
-// Rutas
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
-// Iniciar servidor
+
 app.listen(3000, () => {
 console.log('Servidor corriendo en http://127.0.0.1:3000');
 });
