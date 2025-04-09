acec60134bc6714aa7587885c1b2c38da9b170e3
Mohammad Nacher Rahman Akter <154415872+Nacherpro02@users.noreply.github.com>
2025-04-09 21:43:17 +0200

    update

--- backend/API/server.js
+++ backend/API/server.js
@@ -4,7 +4,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(cors({
-  origin: 'http://localhost:5500',
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'x-auth-token'],
   credentials: true,
